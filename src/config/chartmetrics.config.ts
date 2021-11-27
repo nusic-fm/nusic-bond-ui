a3a3194f97939708295c569271bb96257118c7b2
logesh2496 <you@example.com>
2021-11-27 22:21:54 +0530

    fix: allow only rinkeyby

--- src/config/chartmetrics.config.ts
+++
@@ -1,8 +0,0 @@
-export const refreshToken =
-  "jvf7CoxvgPYtShdHZPx2N8RM48YJ28WNgD9k8gqH0aNhwmhLZLWhraPAjoQphA5P";
-
-export const chartMetricsMetadata = {
-  nuBTS: {
-    id: 206557,
-  },
-};
--- src/utils/connectors.ts
+++ src/utils/connectors.ts
@@ -3,11 +3,11 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 export const injectedConnector = new InjectedConnector({
   supportedChainIds: [
     // 1, // Mainet
-    3, // Ropsten
+    // 3, // Ropsten
     4, // Rinkeby
     // 5, // Goerli
-    42, // Kovan
-    137, // Polygon
+    // 42, // Kovan
+    // 137, // Polygon
     // parseInt(process.env.REACT_APP_MATIC_CHAIN_ID as string),
   ],
 });

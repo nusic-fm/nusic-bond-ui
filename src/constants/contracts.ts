20d601b51d44e9b4d1a273ea90ae11652d950137
logesh2496 <you@example.com>
2021-11-28 15:11:56 +0530

    fix: change contract addresses

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,25 +1,19 @@
 const contractAddresses = {
   BondNFTManager: {
     42: "0xd0bF81468bE0c2a8e247d101b6b71F7E18E24907",
+    4: "0x4cC90cb0217C13AF94f90ce3a79BfF68c92f68EB",
   },
   BondNftGenerator: {
     42: "0x917555471C809720E8bbd31352676662d7c8b088",
-  },
-  ChainLinkOraclInfo: {
-    42: "0xBE830E4fd86f81Ce0D8A1aC1091FfE802122C05C",
+    4: "0x94F1Da34Dc54661Cd534F5e8197Fe2839f52b603",
   },
   RatingEngine: {
     42: "0x3EA4b78bc6bcbC9F2476288c32cf3E1396D48fb6",
+    4: "0xA7d66A42314878824570AE14EA98312af8eCF799",
   },
   ChainlinkMetadataRequest: {
     42: "0x84F1B27A0F47Bab3840d736E4bb49072c2fC1675",
-  },
-  AuditoryAssetPool: {
-    137: "",
-  },
-  AuditoryRouter: {
-    137: "",
-    42: "",
+    4: "0xF58CCda92BF87C5eFD4b48894901a13e017F672F",
   },
 };
 
